e1e5588b6e72bba0f505e5a95168e9b9dc65d8d5
seok <sparrow422c@gmail.com>
2023-03-31 12:03:14 +0900

    3.31

--- src/main/java/com/example/demo/Answer.java
+++ src/main/java/com/example/demo/Answer.java
@@ -2,27 +2,25 @@ package com.example.demo;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 200)
-    private String subject;
-
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @CreatedDate
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question question;
 }
